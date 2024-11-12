24036e3277102f7684c3e3834d258a70aec4cf18
Avinash-p <avinashpandey456@gmail.com>
2024-11-12 11:39:13 +0530

    Updated dependenices to get the app running

--- demoqa/src/test/java/com/demoqa/AppTest.java
+++ demoqa/src/test/java/com/demoqa/AppTest.java
@@ -1,20 +1,13 @@
 package com.demoqa;
 
-import testng.annotations;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Assert.assertTrue( true );
     }
 }
